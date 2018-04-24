975df774f5d6a9079315c58888e2f8f8f36a0e3c
Thao Le <thao.le@nordpoolgroup.com>
2018-04-24 09:28:37 +0300

    first commit

---
+++ Models/Model.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace vega.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public Make Make { get; set; }    
+    }
+}
